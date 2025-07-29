521f7b24cf352e0adc5eff7841337b6dfb0a34e9
Adrian Batista <albplanas0904@gmail.com>
2025-07-29 18:40:28 -0500

    Enhances data processing and integrates random forest evaluation
    Introduces advanced preprocessing capabilities by adding support for UMAP dimensionality reduction, custom rolling window statistics, and feature engineering. Updates the main function to evaluate a random forest model with specified hyperparameters.
    
    Prepares the foundation for future enhancements by commenting out placeholders for XGBoost evaluation and visualization tools.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from models.random_forest import train_evaluate_random_forest
 from models.training_test_split import split_features_target
 from preprocessing.processing import processing_data
 
@@ -8,15 +9,47 @@ def main():
     target_column = "Q2910"
     dropped_columns = ["Q2933", "Q2934"]
 
-    df = processing_data(data_path, time_column, target_column, dropped_columns)
+    df = processing_data(
+        data_path,
+        time_column,
+        target_column,
+        dropped_columns,
+        algorithm="UMAP",
+        n_components=10,
+        rolling_windows=[5, 10, 20],
+        stats=["mean", "std", "skew", "kurt", "max", "min", "median"],
+    )
 
-    X_train, y_train, X_test, y_test = split_features_target(
+    X_train, y_train, X_test, y_test, train_time, test_time = split_features_target(
         df,
         time_column,
         target_column,
         test_split=0.2,
         strategy="temporal",
     )
+
+    rf_model_temp, _ = train_evaluate_random_forest(
+        X_train, y_train, X_test, y_test, n_estimators=500, max_depth=100, n_jobs=-1
+    )
+    # xgb_model, _= train_evaluate_xgboost(X_train, y_train, X_test, y_test,
+    #                                      n_estimators=500,
+    #                                      max_depth=10,
+    #                                      learning_rate=0.05,)
+
+    # plot_prediction_vs_actual(
+    #     [ rf_model_temp],
+    #     ["Random Forest (Temporal)"],
+    #     X_train, y_train,
+    #     X_test, y_test,
+    #     plot_type='test',  # 'test', 'train', or 'both'
+    #     figsize=(8, 6)
+    # )
+
+    # plot_target_vs_time(X_train, y_train, train_time,
+    #                     X_test, y_test, test_time,
+    #                     [rf_model_temp],
+    #                     ['RF_temp', ],
+    #                     plot_type='test',)
 
 
 if __name__ == "__main__":
