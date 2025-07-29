f3c403784194e1cbb88cc8beb6744b1f56f6ec92
Adrian Batista <albplanas0904@gmail.com>
2025-07-29 13:43:22 -0500

    Adds dimensionality reduction to data processing
    Incorporates a dimensionality reduction step using PCA to streamline datasets by retaining 99% variance. Enhances processing efficiency and supports high-dimensional data handling.

--- preprocessing/processing.py
+++ preprocessing/processing.py
@@ -1,6 +1,7 @@
 import os
 from typing import List
 
+from preprocessing.dimension_reduction import reduce_dimensions
 from preprocessing.handle_nan import treat_nan_dataframe
 from preprocessing.high_correlation import drop_highly_correlated_columns
 from preprocessing.load_timeseries import load_timeseries_file
@@ -37,5 +38,8 @@ def processing_data(
     df = treat_nan_dataframe(df, time_column, k_neighbors=10)
     df = drop_low_variance_columns(df, time_column, target_column, threshold=1e-8)
     df = drop_highly_correlated_columns(df, time_column, target_column, threshold=0.98)
+    df = reduce_dimensions(
+        df, time_column, target_column, method="PCA", pca_variance=0.99
+    )
 
     return df
